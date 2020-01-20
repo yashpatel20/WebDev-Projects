cb923c7c78bc2d5de6bf91d33b870937091ba16c
Yashpatel20 <yash.work10@gmail.com>
2020-01-20 23:57:01 +0530

    TicTacToe-JS

--- TicTacToe-JS/main.js
+++ TicTacToe-JS/main.js
@@ -1,3 +1,38 @@
-//gameBoard and displayController modules
-//player factory functions
+//player factory
+//compose two players in the gameBoard Object
+const player = () =>{
+
+    return{
+
+    };
+};
+
+//gameBoard module
+const game = (()=>{
+    let gameBoard = [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
+
+
+
+
+
+    return{
+        makeBoard,
+        startGame,
+        endGame,
+        disablePlayerTwo
+    };
+})();
+
+//handle buttons
+const startGameBtn = document.getElementsByClassName('btn-newGame');
+const endGameBtn = document.getElementsByClassName('btn-endGame');
+const computerRadioBtn = document.getElementById('checkComputer');
+
+startGameBtn.addEventListener();
+
+
 
