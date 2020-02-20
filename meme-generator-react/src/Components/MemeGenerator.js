c28077e5e56216b9d9594461e57f02e83dad4f97
Yashpatel20 <yash.work10@gmail.com>
2020-02-20 23:57:03 +0530

    fetch api meme generator completed using hooks

--- meme-generator-react/src/Components/MemeGenerator.js
+++ meme-generator-react/src/Components/MemeGenerator.js
@@ -7,14 +7,28 @@ function MemeGenerator(){
     const [randomImg, updateRandomImg] = useState("http://i.imgflip.com/1bij.jpg")
     const [allMemeImgs, updateAllMemeImgs] = useState([]);
 
-    
+    //fetch
     useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response =>{
+                const {memes} = response.data
+                updateAllMemeImgs(memes)
+            })
 
-    })
+    },[])
+
+    const handleFormSubmit = (event)=>{
+        event.preventDefault()
+        //update randomImg with a random meme url from allMemeImgs
+        const randomNum = Math.floor(Math.random()*allMemeImgs.length)
+        const randomUrl = allMemeImgs[randomNum].url
+        updateRandomImg(randomUrl)
+    }
 
     return (
         <div>
-            <form className="meme-form">
+            <form className="meme-form" onSubmit={handleFormSubmit}>
                 <input
                     type="text"
                     name="topText" 
@@ -29,6 +43,7 @@ function MemeGenerator(){
                     value={bottomText}
                     onChange={(e)=>{updateBottomText(e.target.value)}}
                 />
+                <button>Gen</button>
             </form>
             <div className="meme">
                 <img src={randomImg} alt="" />
