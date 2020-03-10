50ed40086aefdf978f9996f3c9446fa77a507be1
Yashpatel20 <yash.work10@gmail.com>
2020-03-10 17:45:07 +0530

    using express for server

--- Odin Course/BasicInformationSite-NodeJS/server.js
+++ Odin Course/BasicInformationSite-NodeJS/server.js
@@ -1,38 +1,28 @@
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
 
-http
-  .createServer((req, res) => {
-    console.log(req.url);
-    switch (req.url) {
-      case "/":
-        fs.readFile("./index.html", (err, data) => {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        });
-        break;
-      case "/about":
-        fs.readFile("./about.html", (err, data) => {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        });
-        break;
-      case "/contact-me":
-        fs.readFile("./contact-me.html", (err, data) => {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        });
-        break;
-      default:
-        fs.readFile("./404.html", (err, data) => {
-          res.writeHead(400, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        });
-        break;
-    }
-  })
-  .listen(8080);
+const app = express();
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/about", (req, res) => {
+  fs.readFile("./about.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+});
+app.get("/contact-me", (req, res) => {
+  fs.readFile("./contact-me.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+});
+
+app.listen(8080);
