c40345998c692314cd5ae4c0e5250bd87110801d
Yashpatel20 <yash.work10@gmail.com>
2020-03-10 15:06:25 +0530

    BasicInformationSite-NodeJS

---
+++ Odin Course/BasicInformationSite-NodeJS/server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    switch (req.url) {
+      case "/":
+        fs.readFile("./index.html", (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        });
+        break;
+      case "/about":
+        fs.readFile("./about.html", (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        });
+        break;
+      case "/contact-me":
+        fs.readFile("./contact-me.html", (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        });
+        break;
+      default:
+        fs.readFile("./404.html", (err, data) => {
+          res.writeHead(400, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        });
+        break;
+    }
+  })
+  .listen(8080);
